2f4e829870a4e387946d05b802f0a5a6c2005c2c
Shihab Shaikh <88572629+shihab-fw11-297@users.noreply.github.com>
2025-07-11 14:31:09 +0530

    Create Jenkinsfile.java

---
+++ Jenkinsfile.java
@@ -0,0 +1,13 @@
+ public class HelloJava {
+
+    public static void main(String[] args) {
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        System.out.println("Hello Java...!");
+        System.out.println("Hello intelliJ idea...!");
+
+
+    }
+}
